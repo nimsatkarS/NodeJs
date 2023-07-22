8b66716e4764172bbd0f7617b1431f3a50f57c86
Sanket_Nimsatkar <121743873+nimsatkarS@users.noreply.github.com>
2023-07-22 21:24:57 +0530

    Using Static and  Using template engine

--- tute9/app.js
+++ tute9/app.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 80;
+
+//Serving the static files
+app.use('/static', express.static('static'))
+
+//set the templet engin pug
+app.set('vies engine', 'pug')
+
+//set the views directory
+app.set('views', path.join(__dirname, 'views'));
+
+//our pug end point
+app.get("/demo", (req, res) => {
+    res.status(404).render('demo', { title: 'Hey sanket', message:'Hello World'})
+});
+
 
 app.get('/', (req, res) => {
     res.status(404).send("This is 404 server run");
